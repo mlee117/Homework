3250fb92ad78b555f0f90635d916ac8f6e04e9dc
mlee117 <mlee3745216@gmail.com>
2023-01-26 20:10:06 -0800

    new

---
+++ 27frame.py
@@ -0,0 +1,32 @@
+# 27frame.py
+
+# Write a program that prints out the position, frame, and letter of the DNA
+
+# Variation: try coding this with a single loop and nested loops
+
+# Note: use 0-based indexing for position and frame (biology uses 1-based)
+
+dna = 'ATGGCCTTT'
+
+for nt in range(len(dna)):
+	frame = nt
+	if frame % 3 == 0:
+		frame = 0
+	elif (frame + 1) % 3 == 0:
+		frame = 2
+	else:
+		frame = 1
+	print(nt, frame, dna[nt])
+
+"""
+python3 27frame.py
+0 0 A
+1 1 T
+2 2 G
+3 0 G
+4 1 C
+5 2 C
+6 0 T
+7 1 T
+8 2 T
+"""
